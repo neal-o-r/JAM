26d4bbd091f77ea8e8bbf5b9d7d14e418be38387
neal-o-r <neal@n-o-r.xyz>
2017-06-03 21:37:45 +0100

    vectorised R

--- simple_gam.jl
+++ simple_gam.jl
@@ -13,7 +13,7 @@ function main()
 	knots = quantile(df[:times], Array(linspace(0, 1, k)))
 
 	t = Array(df[:times])
-	x = hcat(ones(length(df[:times])), t, apply_R(t, knots))
+	x = hcat(ones(length(t)), t, R(t, knots))
 
 	S = zeros(k+2, k+2)	
 	S[3:end, 3:end] = R(knots, knots)
@@ -28,35 +28,16 @@ function main()
 end
 
 
-function R(x::Float64, z::Float64)
+function R(x::Array{Float64, 1}, z::Array{Float64, 1})
 
-	r = (((z - 0.5)^2 - 1 / 12) * 
-	    ((x - 0.5)^2 - 1 / 12) / 4 - 
-	    ((abs(x - z) - 0.5)^4 - 0.5 * 
-	    (abs(x - z)  - 0.5)^2 + 7 / 240) / 24)
+	r = (((z' - 0.5).^2 - 1 / 12) .* 
+	    ((x - 0.5).^2 - 1 / 12) / 4 - 
+	    ((abs(broadcast(-, x, z')) - 0.5).^4 - 0.5 .* 
+	    (abs(broadcast(-, x, z'))  - 0.5).^2 + 7 / 240) / 24)
 	
 	return r
 
 end
-
-
-function apply_R(x::Array{Float64, 1}, z::Array{Float64, 1})
-
-
-	R_mat = ones(length(x), length(z))
-	for (i, xi) in enumerate(x)
-		for (j, zj) in enumerate(z)
-		
-			R_mat[i, j] = R(xi, zj)	
-
-		end	
-	end
-
-	return R_mat
-
-
-end
-
 
 
 function fit_model(X::Array{Float64, 2}, Y::Array{Float64, 1}, 
