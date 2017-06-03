35b34e31f16a211fce259dd429f75f2b2d8a3ea9
neal-o-r <neal@n-o-r.xyz>
2017-06-03 22:30:43 +0100

    added GCV scoring

--- simple_gam.jl
+++ simple_gam.jl
@@ -4,7 +4,7 @@ using DataFrames
 function main()
 
 	df = readtable("motorcycle.csv")
-	scatter(df[:times], df[:accel], color="0.75", alpha=0.8)
+	scatter(df[:times], df[:accel], color="0.75", alpha=0.8, s=3)
 	xlabel("times")
 	ylabel("Acceleration")
 
@@ -21,7 +21,10 @@ function main()
 	B = zeros(k+2, k+2)
 	B[3:end, 3:end] = real(sqrtm(S[3:end, 3:end]))
 
-	y_bar = fit_model(x, Array(df[:accel]), B, 10., k)
+	y_bar, score, lambda = fit_model(x, Array(df[:accel]), B, k)
+	println("Lambda: ", lambda, 
+		"\nScore: ", score)
+
 	plot(t, y_bar, color="k")
 
 
@@ -39,15 +42,50 @@ function R(x::Array{Float64, 1}, z::Array{Float64, 1})
 
 end
 
+function GCV_score(X::Array{Float64, 2}, Y::Array{Float64, 1}, 
+			Y_hat::Array{Float64, 1})
 
-function fit_model(X::Array{Float64, 2}, Y::Array{Float64, 1}, 
-		B::Array{Float64, 2}, lambda::Float64, k::Int64)
+	n = size(X)[2]
+	In = (X * inv(X' * X) * X')
+	res = sum((Y - Y_hat).^2) 
+	H = In[1:n, 1:n]
+
+	return n * res  / (n - trace(H))^2
+
+end
+
+
+function solve_ols(X::Array{Float64, 2}, Y::Array{Float64, 1}, 
+	B::Array{Float64, 2}, 
+	lambda::Float64, k::Int64)
 
 	Y_ = vcat(Y, zeros(k+2))
 	X_ = vcat(X, sqrt(lambda) * B)
+	Y_bar = (X * inv(X_' * X_) * X_') * Y_
 
-	return (X * inv(X_' * X_) * X_') * Y_
+	g = GCV_score(X_, Y, Y_bar)
+	
+	return Y_bar, g
 
+end
+
+function fit_model(X::Array{Float64, 2}, Y::Array{Float64, 1}, 
+		B::Array{Float64, 2}, k::Int64)
+
+	lambdas =logspace(-3, 3, 20)
+	
+	gcvs = zeros(20)
+	for (i, l) in enumerate(lambdas)
+	
+		_, s = solve_ols(X, Y, B, l, k)
+		gcvs[i] = s
+
+	end
+	
+	lambda = lambdas[indmin(gcvs)]
+	Y_hat, score = solve_ols(X, Y, B, lambda, k)
+	
+	return Y_hat, score, lambda
 
 end
 
